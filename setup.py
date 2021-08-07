7f04e6158b23a046b4e31767b4b1f077ae6e2cf8
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-08-07 20:26:34 +0800

    Commit blank template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Python based project for performing sentiment analysis on Twitter data.',
+    author='Sarah Hannes',
+    license='MIT',
+)
