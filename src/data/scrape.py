2f1922bc346669421608b83f19fd5edc57db2873
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-08-08 22:04:55 +0800

    Change all single quote to double

--- src/data/scrape.py
+++ src/data/scrape.py
@@ -7,15 +7,15 @@ nest_asyncio.apply()
 # Extract scrape queries
 f = open("src\data\scrape_keyword.txt", "r")
 for line in f:
-    if 'Start Date' in line:
+    if "Start Date" in line:
         start_date = re.findall(r"\d{4}-\d{2}-\d{2}", line)[0]
-    if 'End Date' in line:
+    if "End Date" in line:
         end_date = re.findall(r"\d{4}-\d{2}-\d{2}", line)[0]
-    if 'Hashtag' in line:
-        hashtag = '#'+re.findall(r"[^Hashtag](\w+)", line)[0]
+    if "Hashtag" in line:
+        hashtag = "#"+re.findall(r"[^Hashtag](\w+)", line)[0]
 f.close()
 
-filename = "data\\new\\new_"+''.join(re.findall(r"\d+", end_date))+'.csv'
+filename = "data\\new\\new_"+"".join(re.findall(r"\d+", end_date))+".csv"
 
 # Initialize configuration
 scraper = twint.Config()
@@ -23,7 +23,7 @@ scraper = twint.Config()
 # Configuration paramaters
 scraper.Store_csv = True # stores as csv
 scraper.Output = filename # name of the output file
-scraper.Lang = 'en'
+scraper.Lang = "en"
 scraper.Since = start_date
 scraper.Until = end_date
 scraper.Search = hashtag # query keywords
