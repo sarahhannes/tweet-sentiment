f2ca1ff563b401ef390b597e4e97894e18a642ae
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-09-05 23:59:46 +0800

    Scrape hourly

---
+++ src/data/scrape_hourly.py
@@ -0,0 +1,31 @@
+"""
+Scrape twitter hourly for keyword specified in `hashtag`
+"""
+
+# Import libraries
+from datetime import datetime, timedelta
+import twint
+import nest_asyncio
+nest_asyncio.apply()
+
+# Scrape
+now = datetime.utcnow()
+before = now - timedelta(minutes=60)
+
+# Initialize configuration
+d = twint.Config()
+d.Lang = 'en'
+
+d.Since = before.strftime('%Y-%m-%d %H:%M:%S')
+d.Until = now.strftime('%Y-%m-%d %H:%M:%S')
+
+d.Search = "#dhl"  # what to query
+
+# Run
+twint.run.Search(d)
+
+# Save into csv
+df = twint.storage.panda.Tweets_df
+
+# Write to file (if file not found, otherwise, append)
+df.to_csv(f"raw_data.txt", index=False, sep="\t", mode='a+')
