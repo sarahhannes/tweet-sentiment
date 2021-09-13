872a5f473dac8c5089d926fac455ba9498f06134
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-09-13 13:07:29 +0800

    Store output from gpg cmd as variable

--- .github/scripts/decrypt_secret_runPy.sh
+++ .github/scripts/decrypt_secret_runPy.sh
@@ -6,12 +6,12 @@ mkdir $HOME/secrets
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --passphrase="$CLIENT_SECRET_PASSPHRASE" \
---output $HOME/secrets/client_secret.json -d client_secret.json.gpg
+JSON_OUTPUT=$(gpg --quiet --batch --yes --passphrase="$CLIENT_SECRET_PASSPHRASE" \
+--output $HOME/secrets/client_secret.json -d client_secret.json.gpg)
 
 # Install required dependencies for python script
 pip install google-api-python-client
 pip install google-auth-oauthlib
 
 # Run upload script
-python3 ./src/data/upload_to_gdrive_sys_argv.py $HOME/secrets/client_secret.json
+python3 ./src/data/upload_to_gdrive_sys_argv.py $JSON_OUTPUT
