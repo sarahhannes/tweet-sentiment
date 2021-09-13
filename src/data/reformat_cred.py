d92dd045d12ad26c7d0132ec39291ffc6e7aaaba
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-09-13 15:19:05 +0800

    Change credential output from .sh into correct format

---
+++ src/data/reformat_cred.py
@@ -0,0 +1,8 @@
+import codecs
+import json
+import sys
+
+JSON_OUTPUT = sys.argv[1]
+
+with codecs.open('cred2.json', 'w', 'utf8') as f:
+    f.write(json.dumps(json.loads(JSON_OUTPUT), sort_keys=True, ensure_ascii=False))
