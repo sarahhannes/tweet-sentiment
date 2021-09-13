66da520e8c6f311a25259691d5ae0a19a5425083
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-09-13 15:23:15 +0800

    Correct filename

--- src/data/reformat_cred.py
+++ src/data/reformat_cred.py
@@ -4,5 +4,5 @@ import sys
 
 JSON_OUTPUT = sys.argv[1]
 
-with codecs.open('cred2.json', 'w', 'utf8') as f:
+with codecs.open('client_secret.json', 'w', 'utf8') as f:
     f.write(json.dumps(json.loads(JSON_OUTPUT), sort_keys=True, ensure_ascii=False))
