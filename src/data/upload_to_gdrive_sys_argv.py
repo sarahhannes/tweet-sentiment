ad211e745e7e7d50427ca56af72f43071490d3b2
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-09-13 18:28:06 +0800

    Change to from_client_config(sys.argv, ...)

--- src/data/upload_to_gdrive_sys_argv.py
+++ src/data/upload_to_gdrive_sys_argv.py
@@ -41,7 +41,7 @@ def Create_Service(client_secret_file, api_name, api_version, *scopes):
             cred.refresh(Request())
         else:
             #flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-            flow = InstalledAppFlow.from_client_config(CLIENT_SECRET_FILE,scopes)
+            flow = InstalledAppFlow.from_client_config(CLIENT_SECRET_FILE, scopes)
             cred = flow.run_local_server()
 
         with open(os.path.join(working_dir, token_dir, pickle_file), 'wb') as token:
