1aa464bcc600408615e5297bc366e2e622e25a90
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-09-13 15:15:51 +0800

    Change cred source

--- src/data/upload_to_gdrive_sys_argv.py
+++ src/data/upload_to_gdrive_sys_argv.py
@@ -40,8 +40,8 @@ def Create_Service(client_secret_file, api_name, api_version, *scopes):
         if cred and cred.expired and cred.refresh_token:
             cred.refresh(Request())
         else:
-            #flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-            flow = InstalledAppFlow.session_from_client_config(CLIENT_SECRET_FILE, scopes)
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            #flow = InstalledAppFlow.session_from_client_config(CLIENT_SECRET_FILE, scopes)
             cred = flow.run_local_server()
 
         with open(os.path.join(working_dir, token_dir, pickle_file), 'wb') as token:
@@ -62,22 +62,7 @@ def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
     dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + 'Z'
     return dt
 
-
-#print('sys.argv list', [os.fsencode(arg) for arg in sys.argv])
-print('Type of sys.argv[0]:', type(sys.argv[0]))
-print('Type of sys.argv[1]:', type(sys.argv[1]))
-print('The first 100 char for sys.argv[0]:', sys.argv[0][:100])
-#print('The first 100 char for sys.argv[1]:', sys.argv[1][:100])
-CLIENT_SECRET_FILE = json.loads(sys.argv[1])
-print('Type after conversion:', sys.argv[0])
-# print(sys.argv[0])
-# with open(sys.argv[0], 'rb') as file:
-#     print('Input file not empty:', b"installed" in file.read(30))
-#     print('Found client_id in input file:', b"client_id" in file.read(100))
-#     print('Passed file')
-#     #pprint.pprint(json.load(file))
-#     CLIENT_SECRET_FILE = json.loads(file.read())
-
+CLIENT_SECRET_FILE = 'client_secret.json'
 API_NAME = 'drive'
 API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/drive']
