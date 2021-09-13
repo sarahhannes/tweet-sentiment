2d4f18186bff3ae97e41420743e167c600712304
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-09-13 11:59:00 +0800

    Run upload py script in sh

---
+++ .github/scripts/decrypt_secret_runPy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Decrypt the file
+
+mkdir $HOME/secrets
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --passphrase="$CLIENT_SECRET_PASSPHRASE" \
+--output $HOME/secrets/client_secret.json -d client_secret.json.gpg
+
+# Install required dependencies for python script
+pip install google-api-python-client
+pip install google-auth-oauthlib
+
+# Run upload script
+python3 $HOME/src/data/upload_to_gdrive.py $HOME/secrets/client_secret.json
