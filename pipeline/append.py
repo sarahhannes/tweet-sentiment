f680f30551e6e3c6d0886add9656748fd824f2b0
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-09-15 13:02:24 +0800

    Append new (git repo) + existing data (gdrive)

---
+++ pipeline/append.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import gdown
+
+if __name__ == "__main__":
+
+    ## LOAD
+    # Load new data
+    NEWDATA_URL = 'https://raw.githubusercontent.com/SarahHannes/tweet-sentiment/dev/new_data_cleaned.csv'
+    df_new = pd.read_csv(NEWDATA_URL)
+
+    # Load existing data
+    OLDDATA_FILE_ID = '1XiABfco1-NpSwSjl32BAUS_HqPrBFYzD'
+    OLDDATA_URL = 'https://drive.google.com/uc?id=' + OLDDATA_FILE_ID
+    OLDDATA_OUTPUT = 'data.txt'
+    # Download model from google drive
+    gdown.download(OLDDATA_URL, OLDDATA_OUTPUT, quiet=False)
+    df_old = pd.read_csv('data.txt', sep='\t')
+
+    ## TRANSFORM
+    # Merge dataframes & remove duplicates
+    df = pd.concat([df_old, df_new]).reset_index(drop=True).drop_duplicates(subset=['username', 'tweet'])
+
+    ## SAVE
+    df.to_csv('data.txt', sep='\t', index=False)
