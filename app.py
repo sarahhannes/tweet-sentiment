820c2ad9d05d6bdd72e8f84fc0472c4a327da805
SarahHannes <78901374+SarahHannes@users.noreply.github.com>
2021-10-20 09:13:23 +0800

    Localize then convert tz

--- app.py
+++ app.py
@@ -999,7 +999,7 @@ def main():
         filtered_agg_df = agg_df.loc[(agg_df['datetime'] <= f'{selected_week.year}-{selected_week.month}-{selected_week.day} 23:59:59')].copy()
         # Change column type
         #filtered_agg_df['datetime'] = filtered_agg_df['datetime'].apply(lambda x: pd.to_datetime(x).tz_localize('UTC').tz_convert(local_tz))
-        filtered_agg_df['datetime'] = filtered_agg_df['datetime'].apply(lambda x: pd.to_datetime(x).tz_convert(local_tz).tz_convert(server_tz))
+        filtered_agg_df['datetime'] = filtered_agg_df['datetime'].apply(lambda x: pd.to_datetime(x).tz_localize(local_tz).tz_convert(server_tz))
         # Get date column
         filtered_agg_df['date'] = filtered_agg_df['datetime'].apply(lambda x: pd.to_datetime(x).date())
 
