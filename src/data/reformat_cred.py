b58947c6bcd3a9a486cff34630dfc0322279196f
Sarah Hannes <78901374+SarahHannes@users.noreply.github.com>
2021-11-14 15:34:31 +0800

    Delete src folder

--- src/data/reformat_cred.py
+++
@@ -1,8 +0,0 @@
-import codecs
-import json
-import sys
-
-JSON_OUTPUT = sys.argv[1]
-
-with codecs.open('client_secret.json', 'w', 'utf8') as f:
-    f.write(json.dumps(json.loads(JSON_OUTPUT), sort_keys=True, ensure_ascii=False))
