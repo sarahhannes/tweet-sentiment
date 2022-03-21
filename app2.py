403c09af9bbc9c2a19b6529a589130e6890ea1ab
Sarah Hannes <78901374+SarahHannes@users.noreply.github.com>
2022-03-21 20:56:08 +0800

    Set Home DataTable `width=4000`

--- app2.py
+++ app2.py
@@ -1881,7 +1881,7 @@ def main():
                           reorderable=True,
                           scroll_to_selection=True,
                           sortable=True,
-                          selectable="checkbox", aspect_ratio='auto', autosize_mode='fit_viewport')#width=735)
+                          selectable="checkbox", aspect_ratio='auto', width=4000)
             
             # Get event dict containing data from user input on rendered bokeh_plot
             # ref: https://github.com/ash2shukla/streamlit-bokeh-events/blob/master/streamlit_bokeh_events/__init__.py#L21
